c0ab8ff687c92ce2db02b39915a63ea639783713
Pavishy <78425614+Pavishy@users.noreply.github.com>
2021-10-24 20:33:54 -0500

    Actualiza

---
+++ PM_0_ProgramasClase_U3/PT1U3_Random_N_al_M.asm
@@ -0,0 +1,48 @@
+TITLE ANDOMRANGE 5 a 10 con limite de 20
+
+;DESCRPICIÓN 
+;Objetivo: R
+; Autore(s):
+; Semestre: 8vo Semestre ISC 
+;FIN DESCRIPCIÓN
+
+INCLUDE Irvine32.inc
+
+.data
+
+.code
+    menu PROC
+        call Clrscr
+        mov eax, 5
+        mov ebx, 10
+        mov ecx, 20 ;limite
+
+        L1:
+            push eax            ; doesn't change range
+            push ebx
+
+            call BetterRandomRange
+
+            pop ebx
+            pop eax
+            
+        Loop L1
+
+        call WaitMsg
+
+
+    BetterRandomRange:
+        sub ebx, eax        ;mov 400 a ebx
+        xchg ebx, eax        ;random funciona con eax
+        call RandomRange    ; genera el random como entero
+        neg ebx              
+        sub eax, ebx        ;sub 5 desde eax a definir el rango
+        call WriteInt        ; escribir con signo si es decimal
+        call Crlf
+        ret
+   
+
+    exit
+
+menu endp
+end menu
